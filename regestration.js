c806fba0c53e788212757d82b74a1a6eae116cd4
WA3D2000 <wtaha3113@gmail.com>
2024-11-19 23:18:52 +0200

    Add files via upload

--- regestration.js
+++ regestration.js
@@ -1,29 +1,39 @@
 let email = document.getElementById("em");
 let fn = document.getElementById("FN");
+let massage= document.getElementsByTagName("p")[0];
+let password =document.getElementById("ps");
+
 function validEmail(email) {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     email.match(emailPattern)}
 
-email.addEventListener("mouseenter", () => {
+email.addEventListener("blur", () => {
     const emailValue = email.value;
     if (validEmail(emailValue)) {
         console.log("Valid email address");
     }
 });
-let password =document.getElementById("ps");
-password.addEventListener("mouseenter", () => {
+
+
+password.addEventListener("blur", () => {
     const passwordValue = password.value; 
     const passwordLength = 6;
-
+    
     if (passwordValue.length < passwordLength) {
-        console.log("You should enter at least 6 characters");
+      let  node = document.createTextNode("You should enter at least 6 characters");
+      massage. appendChild( node);
+      console.log(massage);
     }
 });
-let submition=document.getElementById("sub")
+
+let submition=document.getElementById("sub");
 submition.addEventListener("click", () => {
-         localStorage.setItem("email",email)
-         localStorage.setItem("password",password)
-         localStorage.setItem("fullname",fn)
+         
+    localStorage.setItem("email",email.value)
+    localStorage.setItem("password",password.value)
+    localStorage.setItem("fullname",fn.value)
+
+        
 
 });
 
